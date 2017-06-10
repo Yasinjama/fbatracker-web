bed806a214f63f471aba72a15f9c57dc94b1b3ea
Yasin Mohamed <YasinMo@Yasins-MacBook-Pro.local>
2017-06-10 14:00:52 -0500

    adding register method

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-model: any = {};
+ model: any = {};
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  register(){}
 }
