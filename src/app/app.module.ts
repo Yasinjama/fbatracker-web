8877ea5872ce4da777b5a8815ac065158d6d9064
Yasin Mohamed <YasinMo@Yasins-MacBook-Pro.local>
2017-06-10 13:32:02 -0500

    adding registeration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 import {routing} from './app.routing';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
-//import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './register/register.component';
 
 const appRoutes: Routes = [
                            
                            { path: 'login', component: LoginComponent },
-//                           { path: 'register', component: RegisterComponent },
+                           { path: 'register', component: RegisterComponent },
 
                            // otherwise redirect to home
                            { path: '**', redirectTo: '' }
